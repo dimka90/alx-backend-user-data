a45a7ac62b3e5e8932beaa156ad2119f698077cd
dimka90 <dimkayilrit@gmail.com>
2024-04-10 22:57:24 +0100

    used the List data structure from the typing class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,9 @@
 """A personal Data module"""
 
 import re
+from typing import List
 
-
-def filter_datum(fields: list, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """A function that replace a message with some random values"""
     for field in fields:
