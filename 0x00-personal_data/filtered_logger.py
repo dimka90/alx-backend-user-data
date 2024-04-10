d6b7a4411efb73464bba140791e9bb8a63ce82e1
dimka90 <dimkayilrit@gmail.com>
2024-04-10 23:00:33 +0100

    fix some bug, by reducing the line of code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,9 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """A function that replace a message with some random values"""
     for field in fields:
         message = re.sub("{}=.*?{}".format(field, separator),
