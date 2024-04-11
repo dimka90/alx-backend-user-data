f57b4f8aff3e108f6cc17b096cedd3fc252afa24
dimka90 <dimkayilrit@gmail.com>
2024-04-11 15:17:48 +0100

    annoted the get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,7 +54,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MYSQLConnection:
     """Connect the Database using the environment vairable"""
     connector = mysql.connector.connect(
         user=os.getenv("PERSONAL_DATA_DB_USERNAME", 'root'),
