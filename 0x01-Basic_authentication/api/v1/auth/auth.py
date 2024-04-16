44823838d1f9ef26a7d2bc591a967c76e33a3606
dimka90 <dimkayilrit@gmail.com>
2024-04-16 05:55:29 +0100

    fix pycodestyle erorr

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,14 +7,14 @@ from typing import List, TypeVar
 class Auth:
     """Authentication template"""
 
-    def __init__(self):
-        """Object instantiation"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Authentication method"""
         return False
+
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         return
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""
         return
