8c312d71475f87941ac7abe90185fec6f16b3ff5
dimka90 <dimkayilrit@gmail.com>
2024-04-19 05:54:24 +0100

    user authentication

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,23 +56,11 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Returns a connector to a MySQL database """
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-
-    cnx = mysql.connector.connection.MySQLConnection(user=username,
-                                                     password=password,
-                                                     host=host,
-                                                     database=db_name)
-    return cnx
-# def get_db() -> mysql.connector.connection.MySQLConnection:
-#     """Connect the Database using the environment vairable"""
-#     connector = mysql.connector.connect(
-#         user=os.getenv("PERSONAL_DATA_DB_USERNAME", 'root'),
-#         password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-#         host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-#         database=os.getenv("PERSONAL_DATA_DB_NAME")
-#     )
-#     return connector
+    """Connect the Database using the environment vairable"""
+    connector = mysql.connector.connect(
+        user=os.getenv("PERSONAL_DATA_DB_USERNAME", 'root'),
+        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=os.getenv("PERSONAL_DATA_DB_NAME")
+    )
+    return connector
