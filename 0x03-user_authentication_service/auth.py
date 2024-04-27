c58487d5eeac3585c419c39f631db07f56e25967
dimka90 <dimkayilrit@gmail.com>
2024-04-27 16:53:16 +0100

    added a function that checks if a user register or not before registering

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,9 @@ Module that Authenticate a User
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -20,3 +23,25 @@ def _hash_password(password: str) -> bytes:
     password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
     return password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        """
+        try:
+            is_register = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+            pass
+        hashed_password = _hash_password(password)
+
+        user = self._db.add_user(email, hashed_password)
+
+        return user
