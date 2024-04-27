14707a7591ac34f9949a8eabf96123a6f77c61c2
dimka90 <dimkayilrit@gmail.com>
2024-04-27 15:02:08 +0100

    created a function that searches for a user in the database and return the result or NoResultFound Error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -45,3 +47,24 @@ class DB:
         session.add(add_user)
         session.commit()
         return add_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        search for a user in the database
+        Argument:
+                kwargs(dict): a key, value pair for email and
+                hashed_password
+        Return:
+                User(instance)
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+
+        requested_user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not requested_user:
+            raise NoResultFound
+        return requested_user
